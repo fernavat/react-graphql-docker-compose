85c7c06e2c23d567bbf3636a8b4a492a5ae2c9e3
fernavat <fernavat@gmail.com>
2018-07-04 15:17:59 +0000

    Filtering

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -3,18 +3,20 @@ import LinkList from './LinkList'
 import CreateLink from './CreateLink'
 import Header from './Header'
 import Login from './Login'
+import Search from './Search'
 import { Switch, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      <div className="center w85">
+      <div className='center w85'>
         <Header />
-        <div className="ph3 pv1 background-gray">
+        <div className='ph3 pv1 background-gray'>
           <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/" component={LinkList} />
-            <Route exact path="/create" component={CreateLink} />
+            <Route exact path='/' component={LinkList} />
+            <Route exact path='/create' component={CreateLink} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/search' component={Search} />
           </Switch>
         </div>
       </div>
